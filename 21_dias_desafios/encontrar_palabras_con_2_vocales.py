ea80726b658866ad4a3738413838346170cb8a4c
BoFo <brunofazio466@gmail.com>
2023-08-16 15:44:44 -0300

    encontrar palabras con list comprehension

---
+++ 21_dias_desafios/encontrar_palabras_con_2_vocales.py
@@ -0,0 +1,27 @@
+""" En este desafío, debes crear la lógica de la función find_words_with_two_vowels que encuentre las palabras que contienen exactamente dos vocales en una lista de palabras. Las vocales pueden ser tanto mayúsculas como minúsculas.
+
+Recibirás un único parámetro: una lista de palabras. La función debe devolver una nueva lista que contenga todas las palabras de la lista original que cumplan con la condición mencionada anteriormente. En caso de no haber palabras que cumplan con esta condición deberás retornar una lista vacía.
+
+Ejemplo 1:
+
+Input: find_words_with_two_vowels([
+  "hello",
+  "Python",
+  "world",
+  "platzi"
+])
+
+Output: ["hello", "platzi"]
+
+Ejemplo 2:
+
+Input: find_words_with_two_vowels(["text", "test", "python", "example"])
+Output: [] """
+
+# solucion utilizando el metodo len
+words = ["hello", "Python", "world", "platzi"]
+def find_words_with_two_vowels(words):
+    words_with_two_vowels = [palabra for palabra in words if len([letra for letra in palabra if letra in "aeiouAEIOU"]) == 2]
+    return words_with_two_vowels
+
+print(find_words_with_two_vowels(words))
